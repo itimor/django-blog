84ec3c24937275609b2e77a3ba4ef41b999d3116
kevin <kevin@126.com>
2018-05-28 18:55:19 +0800

    fix url add edit

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@
 from django.conf.urls import url
 from blog.views import IndexView, BlogDetailView, ArchiveView, TagView, LinkView, GustView, SearchView
 from blog.views import ArticleAddView
-from blog.views import bad_request, permission_denied, page_not_found, server_error
+from blog.views import bad_request, permission_denied, page_not_found, permission_denied
 
 # 定义错误跳转页面
 handler400 = bad_request
@@ -15,9 +15,9 @@ handler500 = bad_request
 app_name = 'blog'
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
-    url(r"^post/(?P<slug>[\w,-]+)", BlogDetailView.as_view(), name="detail"),
+    url(r"^post/view/(?P<slug>[\w,-]+)", BlogDetailView.as_view(), name="detail"),
     url(r"^post/add", ArticleAddView.as_view(), name="add"),
-    url(r"^post/(?P<slug>[\w,-]+)/edit", BlogDetailView.as_view(), name="edit"),
+    url(r"^post/edit/(?P<slug>[\w,-]+)", BlogDetailView.as_view(), name="edit"),
     url(r'^archive/', ArchiveView.as_view(), name="archive"),
     url(r'^tag/(?P<tag>\w+)', TagView.as_view(), name="tag"),
     url(r'^link', LinkView.as_view(), name="link"),
