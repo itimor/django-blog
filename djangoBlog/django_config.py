a4f5f8df7c939fe25daade5e84519a27576b2978
kevin <kevin@126.com>
2018-06-07 16:27:06 +0800

    fix tag

---
+++ djangoBlog/django_config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# author: itimor
+
+EMAIL_INFO = {
+    'EMAIL_HOST': "smtp.126.com",
+    'EMAIL_PORT': "587",
+    'EMAIL_HOST_USER': "itimor@mail.com",
+    'EMAIL_HOST_PASSWORD': "jay541430183",
+    'DEFAULT_FROM_EMAIL': "djangoblog <djangoblog@itimor.cs>",
+}
