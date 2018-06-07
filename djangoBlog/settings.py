5e29961f9744b32505811df58b275188e2c16dfc
kevin <kevin@126.com>
2018-06-07 12:05:56 +0800

    add sites

--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -15,6 +15,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'taggit',           # 第三方tag
     'mdeditor',
     'blog',
