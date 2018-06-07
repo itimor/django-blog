12fa4a92b6fa46b730f1b47ac4b5061a9f05bcf2
kevin <kevin@126.com>
2018-06-07 19:11:33 +0800

    fix archive

--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -102,6 +102,11 @@ USE_TZ = True
 STATIC_URL = '/statics/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "statics/"),)
 
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder"
+)
+
 MEDIA_URL = '/upload/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'upload/')
 
